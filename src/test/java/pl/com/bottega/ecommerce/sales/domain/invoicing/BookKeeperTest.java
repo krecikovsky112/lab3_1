05ef68925c8a4a7759fad41b5de872abf132ae80
krecikovsky112 <krecikovsky112@gmail.com>
2021-04-12 19:48:59 +0200

    Add Test for two elements on invoice

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -36,6 +36,9 @@ class BookKeeperTest {
     @Mock
     private ProductData productData2;
 
+    @Mock
+    ProductData productData3;
+
     private ClientData client;
     private InvoiceRequest request;
     private Invoice invoice;
@@ -49,6 +52,7 @@ class BookKeeperTest {
         productData2 = mock(ProductData.class);
         request = new InvoiceRequest(client);
         invoice = new Invoice(Id.generate(), client);
+        productData3 = mock(ProductData.class);
     }
 
     @Test
@@ -91,6 +95,27 @@ class BookKeeperTest {
         bookKeeper.issuance(request, taxPolicy);
 
         assertEquals(invoice.getItems().size(), 0);
+    }
+
+    @Test
+    void shouldReturnInvoiceWithThreePosition() {
+        when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(new Money(BigDecimal.ONE), "tax"));
+
+        when(productData.getType()).thenReturn(ProductType.STANDARD);
+        when(productData2.getType()).thenReturn(ProductType.FOOD);
+
+        RequestItem item = new RequestItem(productData, 1, new Money(3));
+        request.add(item);
+
+        RequestItem item2 = new RequestItem(productData2, 3, new Money(5));
+        request.add(item2);
+
+
+        when(factory.create(client)).thenReturn(invoice);
+
+        bookKeeper.issuance(request, taxPolicy);
+
+        assertEquals(invoice.getItems().size(), 2);
     }
 }
 
