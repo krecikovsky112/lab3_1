e22952d5f5cdbd9d1f1e6fb564a589c1499738bc
krecikovsky112 <krecikovsky112@gmail.com>
2021-04-12 19:31:23 +0200

    Add Test case 2

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
@@ -15,8 +16,7 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BookKeeperTest {
@@ -33,30 +33,56 @@ class BookKeeperTest {
     @Mock
     private ProductData productData;
 
+    @Mock
+    private ProductData productData2;
+
+    private ClientData client;
+    private InvoiceRequest request;
+
     @BeforeEach
-    void setUp(){
-        bookKeeper=new BookKeeper(factory);
-        productData=mock(ProductData.class);
-        taxPolicy=mock(TaxPolicy.class);
+    void setUp() {
+        bookKeeper = new BookKeeper(factory);
+        productData = mock(ProductData.class);
+        taxPolicy = mock(TaxPolicy.class);
+        client = new ClientData(Id.generate(), "Andrzej");
+        productData2 = mock(ProductData.class);
+        request = new InvoiceRequest(client);
     }
 
     @Test
-    void shouldReturnIvoiceWithOnePositon()
-    {
-        ClientData client = new ClientData(Id.generate(),"Andrzej");
-        InvoiceRequest request=new InvoiceRequest(client);
-
-        when(taxPolicy.calculateTax(any(ProductType.class),any(Money.class))).thenReturn(new Tax(new Money(BigDecimal.ONE),"tax"));
+    void shouldReturnIvoiceWithOnePositon() {
+        when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(new Money(BigDecimal.ONE), "tax"));
         when(productData.getType()).thenReturn(ProductType.STANDARD);
 
-        RequestItem item=new RequestItem(productData,1,new Money(3));
+        RequestItem item = new RequestItem(productData, 1, new Money(3));
         request.add(item);
 
-        Invoice invoice = new Invoice(Id.generate(),client);
+        Invoice invoice = new Invoice(Id.generate(), client);
         when(factory.create(client)).thenReturn(invoice);
 
-        bookKeeper.issuance(request,taxPolicy);
-        assertEquals(invoice.getItems().size(),1);
+        bookKeeper.issuance(request, taxPolicy);
+        assertEquals(invoice.getItems().size(), 1);
+    }
+
+    @Test
+    void shouldCallCalculateTaxTwice() {
+        when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(new Money(BigDecimal.ONE), "tax"));
+        when(productData.getType()).thenReturn(ProductType.STANDARD);
+        when(productData2.getType()).thenReturn(ProductType.STANDARD);
+
+        RequestItem item = new RequestItem(productData, 1, new Money(3));
+        request.add(item);
+
+        RequestItem item2 = new RequestItem(productData2, 3, new Money(5));
+        request.add(item2);
+
+        Invoice invoice = new Invoice(Id.generate(), client);
+        when(factory.create(client)).thenReturn(invoice);
+
+        bookKeeper.issuance(request, taxPolicy);
+
+        Mockito.verify(taxPolicy, times(2)).calculateTax(any(ProductType.class), any(Money.class));
     }
 }
+
 
